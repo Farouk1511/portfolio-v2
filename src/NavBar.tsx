4280ba79421c6b6484a5fd3c99780bb2343f293e
Farouk <kazeem.farouk@yahoo.com>
2025-01-28 06:58:24 -0500

    added resposiveness to navabr

--- src/NavBar.tsx
+++ src/NavBar.tsx
@@ -3,59 +3,22 @@ import { NavLink } from "react-router-dom";
 const NavBar = () => {
   return (
     <nav
-      className="flex justify-evenly items-center rounded-tr-2xl rounded-bl-2xl bg-[#2b2b2cbf] w-full sm:w-3/5 h-20 text-[#d6d6d6] p-4"
+      className="fixed sm:static bottom-0 left-0 sm:left-0 z-40 w-full sm:w-full md:w-3/4 lg:w-3/4 xl:w-3/4 h-20 bg-[#2b2b2c] text-[#d6d6d6] flex justify-evenly items-center p-4 sm:p-0 rounded-t-xl sm:rounded-t-xl md:rounded-tr-2xl md:rounded-bl-2xl lg:rounded-tr-2xl lg:rounded-bl-2xl xl:rounded-tr-2xl xl:rounded-bl-2xl"
       aria-label="Main navigation"
     >
+      {["/", "/resume", "/portfolio", "/blog", "/contact"].map((path, index) => (
       <NavLink
-        to={"/"}
+        key={index}
+        to={path}
         className={({ isActive }) =>
-          `cursor-pointer hover:text-[#ffdb70] transition-colors ${
-            isActive ? "text-[#ffdb70]" : ""
-          }`
+        `cursor-pointer hover:text-[#ffdb70] transition-colors ${
+          isActive ? "text-[#ffdb70]" : ""
+        }`
         }
       >
-        About
+        {path === "/" ? "About" : path.substring(1).charAt(0).toUpperCase() + path.substring(2)}
       </NavLink>
-      <NavLink
-        to={"/resume"}
-        className={({ isActive }) =>
-          `cursor-pointer hover:text-[#ffdb70] transition-colors ${
-            isActive ? "text-[#ffdb70]" : ""
-          }`
-        }
-      >
-        Resume
-      </NavLink>
-      <NavLink
-        to={"/portfolio"}
-        className={({ isActive }) =>
-          `cursor-pointer hover:text-[#ffdb70] transition-colors ${
-            isActive ? "text-[#ffdb70]" : ""
-          }`
-        }
-      >
-        Portfolio
-      </NavLink>
-      <NavLink
-        to={"/blog"}
-        className={({ isActive }) =>
-          `cursor-pointer hover:text-[#ffdb70] transition-colors ${
-            isActive ? "text-[#ffdb70]" : ""
-          }`
-        }
-      >
-        Blog
-      </NavLink>
-      <NavLink
-        to={"/contact"}
-        className={({ isActive }) =>
-          `cursor-pointer hover:text-[#ffdb70] transition-colors ${
-            isActive ? "text-[#ffdb70]" : ""
-          }`
-        }
-      >
-        Contact
-      </NavLink>
+      ))}
     </nav>
   );
 };
